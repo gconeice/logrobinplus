c6641e20b364323c834239129bcae48f012cd8f8
gconeice <yyang811@gatech.edu>
2024-05-11 12:07:42 -0400

    test finish

---
+++ test/logrobinplus/test.cpp
@@ -0,0 +1,146 @@
+#include <random>
+#include "robin_utils.h"
+#include "emp-zk/emp-zk.h"
+#include <iostream>
+#include "emp-tool/emp-tool.h"
+#if defined(__linux__)
+	#include <sys/time.h>
+	#include <sys/resource.h>
+#elif defined(__APPLE__)
+	#include <unistd.h>
+	#include <sys/resource.h>
+	#include <mach/mach.h>
+#endif
+
+using namespace emp;
+using namespace std;
+
+int port, party;
+const int threads = 1;
+
+uint64_t comm(BoolIO<NetIO> *ios[threads]) {
+	uint64_t c = 0;
+	for(int i = 0; i < threads; ++i)
+		c += ios[i]->counter;
+	return c;
+}
+uint64_t comm2(BoolIO<NetIO> *ios[threads]) {
+	uint64_t c = 0;
+	for(int i = 0; i < threads; ++i)
+		c += ios[i]->io->counter;
+	return c;
+}
+
+void test_circuit_zk(BoolIO<NetIO> *ios[threads], int party, size_t branch_size, size_t nin, size_t nx) {
+
+    branch_size = 1 << branch_size;
+
+    // testing communication
+    uint64_t com1 = comm(ios);
+	uint64_t com11 = comm2(ios);
+
+    // initial zk exec
+    auto init_start = clock_start();
+    setup_zk_arith<BoolIO<NetIO>>(ios, threads, party);
+    auto init_time = time_from(init_start);
+
+    // set up randomized disjunctive circuits
+    std::random_device::result_type cir_seed;
+    if (party == ALICE) {
+        std::random_device rd; // obtain a random number from hardware
+        cir_seed = rd();
+        ZKFpExec::zk_exec->send_data(&cir_seed, sizeof(std::random_device::result_type));
+    } else {
+        ZKFpExec::zk_exec->recv_data(&cir_seed, sizeof(std::random_device::result_type));
+    }
+    auto cir_gen = std::mt19937(cir_seed);
+
+    // generate random circuits for disjunctive statement
+    std::vector<Circuit> cir;
+    for (size_t bid = 0; bid < branch_size; bid++) {
+        cir.push_back(Circuit(nin, nx));
+        cir[cir.size()-1].rand_cir(cir_gen);
+    }
+
+    // for P to generate a random witness
+    f61 final_res;
+    std::vector<f61> win, wl, wr, wo;
+
+    // test for a single circuit now
+    if (party == ALICE) {
+        std::random_device rd; // obtain a random number from hardware
+        auto wit_seed = rd();
+        auto wit_gen = std::mt19937(wit_seed);
+        final_res = cir[0].f61_gen_wit(wit_gen, win, wl, wr, wo);
+        ZKFpExec::zk_exec->send_data(&final_res, sizeof(f61));
+    } else {
+        ZKFpExec::zk_exec->recv_data(&final_res, sizeof(f61));
+    }
+
+    auto start = clock_start();
+
+    // Alice commit the input
+    std::vector<IntFp> com_in;
+    for (size_t i = 0; i < nin; i++) com_in.push_back( IntFp(party == ALICE ? win[i].val : 0, ALICE) );
+
+    IntFp com_out = cir[0].f61_zk_eval(com_in);
+
+    com_out = com_out + (PR - final_res.val);
+    batch_reveal_check_zero(&com_out, 1);
+
+	finalize_zk_arith<BoolIO<NetIO>>();
+	auto timeuse = time_from(start);	
+	cout << init_time+timeuse << " us\t" << party << " " << endl;
+	std::cout << std::endl;
+
+	uint64_t com2 = comm(ios) - com1;
+	uint64_t com22 = comm2(ios) - com11;
+	std::cout << "communication (B): " << com2 << std::endl;
+	std::cout << "communication (B): " << com22 << std::endl;
+
+#if defined(__linux__)
+	struct rusage rusage;
+	if (!getrusage(RUSAGE_SELF, &rusage))
+		std::cout << "[Linux]Peak resident set size: " << (size_t)rusage.ru_maxrss << std::endl;
+	else std::cout << "[Linux]Query RSS failed" << std::endl;
+#elif defined(__APPLE__)
+	struct mach_task_basic_info info;
+	mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+	if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO, (task_info_t)&info, &count) == KERN_SUCCESS)
+		std::cout << "[Mac]Peak resident set size: " << (size_t)info.resident_size_max << std::endl;
+	else std::cout << "[Mac]Query RSS failed" << std::endl;
+#endif
+}
+
+int main(int argc, char** argv) {
+
+	size_t branch_size = 0;
+	size_t nin = 0;
+    size_t nx = 0;
+	if(argc < 7) {
+		std::cout << "usage: exe PARTY PORT IP LOG_BRANCH_SIZE CIR_IN_SIZE CIR_MULT_SIZE" << std::endl;
+		return -1;
+	} else {
+        branch_size = atoi(argv[4]);
+        nin = atoi(argv[5]);
+        nx = atoi(argv[6]);
+	}
+
+	parse_party_and_port(argv, &party, &port);
+	BoolIO<NetIO>* ios[threads];
+	for(int i = 0; i < threads; ++i)
+		ios[i] = new BoolIO<NetIO>(new NetIO(party == ALICE?nullptr:argv[3],port+i), party==ALICE);
+
+	std::cout << std::endl << "------------ circuit zero-knowledge proof test ------------" << std::endl << std::endl;;
+
+
+	
+	test_circuit_zk(ios, party, branch_size, nin, nx);
+
+	for(int i = 0; i < threads; ++i) {
+		delete ios[i]->io;
+		delete ios[i];
+	}
+	return 0;
+
+}
